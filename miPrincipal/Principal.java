88a8d61a66c26042cffd671c8526565a6e0374dc
Jorge Emilio Aguilar Guerrero <159278343+JorgeEmilioAguilarGuerrero@users.noreply.github.com>
2024-02-26 15:05:55 +0000

    Actualizacion

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -4,6 +4,6 @@ package miPrincipal;
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo, soy Lourdes"); //Esto es un comentario
+        System.out.println("Hola Mundo, soy Jorge"); //Esto es un comentario
     }
 }
---
+++ miPrincipal/Programa.java
@@ -0,0 +1,22 @@
+package miPrincipal;
+//Declaramos import
+import otrosProgramas.*; //Esta sentencia importa todas las clases 
+                         //del paquete otrosProgramas
+public class Programa {
+    public static void main (String[] args) {
+        obtenerDatos();
+        alfabetizar();
+        verpalabras();
+        verpalabras();
+        verpalabras();
+    }
+    public static void obtenerDatos() {
+        System.out.println("Estoy en la funcion obtener datos");
+    }
+    public static void alfabetizar() {
+        System.out.println("Estoy en la funcion alfabetizar");
+    }
+    public static void verpalabras() {
+        System.out.println("Estoy en la funcion obtener ver palabras");
+    }
+}
