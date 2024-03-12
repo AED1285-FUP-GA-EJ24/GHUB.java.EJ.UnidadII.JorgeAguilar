54e029cad868244910f289987873c2db1b35a700
Jorge Emilio Aguilar Guerrero <159278343+JorgeEmilioAguilarGuerrero@users.noreply.github.com>
2024-03-12 15:05:54 +0000

    Actualizacion 12 de marzo

---
+++ promedio/Principal.java
@@ -0,0 +1,9 @@
+package promedio;
+
+public class Principal{
+    public static void main(String[] args) {
+        Promedio objPromedio = new Promedio();
+        objPromedio.calcularPromedio();
+    }
+
+}
